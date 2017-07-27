34592e969f6edfef6f644b1e39b75185aa602ced
Ben Timby <btimby@gmail.com>
2017-07-27 13:07:05 -0400

    Genny replacement.

---
+++ filehub/__main__.py
@@ -0,0 +1,13 @@
+"""Genny, audit record generator."""
+
+from __future__ import absolute_import
+
+from filehub import FileHubClient
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
